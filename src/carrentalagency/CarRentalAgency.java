4b04c472a93d7858768d4a81e08db78c0f7d6ed0
marthapals <martha.pals@hotmail.com>
2018-10-29 12:57:21 +0100

    agency

--- src/carrentalagency/CarRentalAgency.java
+++ src/carrentalagency/CarRentalAgency.java
@@ -1,5 +1,13 @@
 package carrentalagency;
 
+import namingservice.INamingService;
+
 public class CarRentalAgency implements ICarRentalAgency{
+
+	private INamingService namingService;
+
+	public CarRentalAgency(INamingService namingService) {
+		this.namingService = namingService;
+	}
 
 }
--- src/carrentalagency/CarRentalAgencyServer.java
+++ src/carrentalagency/CarRentalAgencyServer.java
@@ -1,5 +1,28 @@
 package carrentalagency;
 
-public class CarRentalAgencyServer {
+import java.rmi.AccessException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
 
+import namingservice.INamingService;
+
+public class CarRentalAgencyServer {
+	
+	
+	public static void main(String[] args) throws RemoteException, NotBoundException{
+		System.setSecurityManager(null);
+		Registry registry = LocateRegistry.getRegistry();
+		INamingService namingService = (INamingService) registry.lookup("namingservice");
+		ICarRentalAgency crc = new CarRentalAgency(namingService);
+        ICarRentalAgency stub =
+            ( ICarRentalAgency) UnicastRemoteObject.exportObject(crc, 0);
+        registry.rebind("carrentalagency", stub);
+	}
+	
+	
+	
+	
 }
