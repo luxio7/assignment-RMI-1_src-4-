a24acdc5f9a7ca0d2298b092c889b15b7c0c80c0
marthapals <martha.pals@hotmail.com>
2018-10-29 16:14:26 +0100

    commit

--- src/carrentalagency/CarRentalAgency.java
+++ src/carrentalagency/CarRentalAgency.java
@@ -1,6 +1,15 @@
 package carrentalagency;
 
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.HashMap;
+import java.util.Map;
+
 import namingservice.INamingService;
+import session.IManagerSession;
+import session.IReservationSession;
+import session.ManagerSession;
+import session.ReservationSession;
 
 public class CarRentalAgency implements ICarRentalAgency{
 
@@ -9,5 +18,56 @@ public class CarRentalAgency implements ICarRentalAgency{
 	public CarRentalAgency(INamingService namingService) {
 		this.namingService = namingService;
 	}
-
+	private Map<String, ReservationSession> activeReservationSessions = new HashMap<String, ReservationSession>();
+	private Map<String, ManagerSession> activeManagerSessions = new HashMap<String, ManagerSession>();
+	
+	public void endReservationSession(String sessionId) 
+			throws RemoteException {
+		synchronized (this.activeReservationSessions) {
+			this.activeReservationSessions.remove(sessionId);
+		}
+	}
+	
+	public void endManagerSession(String sessionId) 
+			throws RemoteException {
+		synchronized (this.activeManagerSessions) {
+			this.activeManagerSessions.remove(sessionId);
+		}
+	}
+	
+	public IReservationSession getReservationSession(String sessionId, String clientName)
+			throws RemoteException, IllegalArgumentException {
+		
+		if (sessionId == null) {
+			throw new IllegalArgumentException();
+		}
+		IReservationSession session = this.activeReservationSessions.get(sessionId);
+		
+		if (session != null) {
+			return session;
+		} else {
+			ReservationSession newSession = new ReservationSession(this.namingService, sessionId, clientName);
+			this.activeReservationSessions.put(sessionId, newSession);
+			return (IReservationSession) UnicastRemoteObject.exportObject(newSession, 0);
+		}
+	}
+	
+	public IManagerSession getManagerSession(String sessionId)
+			throws RemoteException, IllegalArgumentException {
+		
+		if (sessionId == null) {
+			throw new IllegalArgumentException();
+		}
+		
+		IManagerSession session = this.activeManagerSessions.get(sessionId);
+		
+		if (session != null) {
+			return session;
+		} else {
+			ManagerSession newSession = new ManagerSession(this.namingService, sessionId);
+			this.activeManagerSessions.put(sessionId, newSession);
+			return (IManagerSession) UnicastRemoteObject.exportObject(newSession, 0);
+		}
+	}
+	
 }
--- src/session/AbstractSession.java
+++ src/session/AbstractSession.java
@@ -1,5 +1,31 @@
 package session;
 
-public class AbstractSession {
+import java.util.Date;
 
+import namingservice.INamingService;
+
+public class AbstractSession {
+	protected String sessionId;
+		
+		protected Date date;		
+		protected INamingService namingService;		
+		protected AbstractSession(INamingService ns, String Id) {
+			this.namingService = ns;
+			this.sessionId = Id;
+			this.date = new Date();
+		}
+		
+		public INamingService getNamingService() {
+			return this.namingService;
+		}
+		
+		public String getSessionId() {
+			return this.sessionId;
+		}
+		
+		public Date getDate() {
+			return this.date;
+		}
+		
 }
+
